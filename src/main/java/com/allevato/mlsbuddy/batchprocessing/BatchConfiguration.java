5bfdac71dab694fc9da9288aa030e7016368e06e
Nick Allevato <nicholas.allevato@gmail.com>
2020-01-02 14:34:50 -0500

    Fix batch processing step

--- src/main/java/com/allevato/mlsbuddy/batchprocessing/BatchConfiguration.java
+++ src/main/java/com/allevato/mlsbuddy/batchprocessing/BatchConfiguration.java
@@ -1,23 +1,25 @@
 package com.allevato.mlsbuddy.batchprocessing;
 
-import com.allevato.mlsbuddy.scraper.data.Property;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
-  @Autowired public JobBuilderFactory jobBuilderFactory;
-  @Autowired public StepBuilderFactory stepBuilderFactory;
+  public final JobBuilderFactory jobBuilderFactory;
+  public final StepBuilderFactory stepBuilderFactory;
+
+  public BatchConfiguration(
+      JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+    this.jobBuilderFactory = jobBuilderFactory;
+    this.stepBuilderFactory = stepBuilderFactory;
+  }
 
   @Bean
   PropertyListReader reader() {
@@ -36,9 +38,7 @@ public class BatchConfiguration {
   }
 
   @Bean
-  public Step step1() {
-    return stepBuilderFactory.get("step1")
-        .reader(reader())
-        .build();
+  public Step step1(PropertyListReader reader) {
+    return stepBuilderFactory.get("step1").chunk(1).reader(reader).build();
   }
 }
